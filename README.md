0f92ecab4b3d756c50454fe721760518b9839bc8
Ivan Chen <IvanChen07@gmail.com>
2021-12-04 00:57:28 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# attendance-scanner
+
+A simplistic python command line script to log attendance. See below for features.
+
+## To use
+
+- Clone the repo. `git clone https://github.com/anivanchen/attendance-scanner`
+- Run the script. A CSV file should automatically be generated for the day. 
+
+## Features
+
+- Automatic file creation for separate days. 
+- Automatic data import allows for the tool to work without duplicates after being run multiple times.
+- Attendance logger.
---
+++ scanner.py
@@ -0,0 +1,51 @@
+# Attendance Scanner
+
+import csv
+import datetime as dt
+from os.path import exists
+
+members = {}
+timeFormat = "%H:%M:%S"
+
+def checkIfNewDay():
+  fileExists = exists(f"attendance/{dt.date.today().isoformat()}.csv")
+
+  if fileExists is False:
+    open(f"attendance/{dt.date.today().isoformat()}.csv", "w").close() # creates file
+
+def getAlreadySignedIn():
+  with open(f"attendance/{dt.date.today().isoformat()}.csv", "r") as file:
+    reader = csv.reader(file, delimiter=",")
+    lines = 0
+    
+    for row in reader: 
+      members[row[0]] = row[1]
+      lines += 1
+
+def checkInUser(): 
+  id = input("SWIPE: ")
+
+  if id == "000000000": print("Stopped Scanning"), exit() # exit code
+
+  if len(id) == 9: # validates input is OSIS number
+
+    if id not in members: # validates no duplicates
+
+      members[id] = dt.datetime.now().strftime("%H:%M:%S")
+
+      with open(f"attendance/{dt.date.today().isoformat()}.csv", "a") as file:
+        file.write(f"{id},{dt.datetime.now().strftime(timeFormat)}\n")
+
+      print("Successfully Scanned")
+
+    else: print("Already Scanned")
+
+  else: print("Invalid ID. Swipe Again.")
+
+if __name__ == "__main__":
+  
+  checkIfNewDay()
+  getAlreadySignedIn()
+
+  while True: 
+    checkInUser()
